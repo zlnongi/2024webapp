1a79adcb5b9336b7aaf2ff524620c3368920bce0
zlnongi <yeossi8@naver.com>
2024-09-04 10:26:50 +0900

    SearchBarView

--- 04vue/vuetest1/src/App.vue
+++ 04vue/vuetest1/src/App.vue
@@ -1,6 +1,7 @@
 <template>
    <NavbarView />
-   <ComponentView v-bind:newdata="data" :isModal="isModal" :num="selectedNum" @openModal="modalOpen" />
+   <SearchBarView :data="data" />
+   <ComponentView v-bind:newdata="data" @openModal="modalOpen" />
    <ModalView v-bind:mydata="data" :isModal="isModal" :num="selectedNum" @closeModal="closeM" />
 </template>
 
@@ -9,6 +10,7 @@ import mdata from './assets/mdata';
 import NavbarView from './components/NavbarView.vue';
 import ModalView from './components/ModalView.vue';
 import ComponentView from './components/ComponentView.vue';
+import SearchBarView from './components/SearchBarView.vue';
 
 export default {
    name: 'appView',
@@ -38,8 +40,89 @@ export default {
       NavbarView: NavbarView,
       ModalView: ModalView,
       ComponentView: ComponentView,
+      SearchBarView: SearchBarView, // key:value가 같으면 한번만 써도 됨
    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$radius: 5px;
+.container {
+   width: 1000px;
+   margin: 0 auto;
+
+   @media screen and (max-width: 790px) {
+      // 미디어 쿼리 ->  790보다 미만이 되어버리면 크기값을 100%로 쓰겠다
+      width: 100%;
+      padding: 0 16px;
+   }
+   img {
+      display: block;
+   }
+}
+
+.cardWrap {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 10px;
+
+   .card {
+      width: calc((100% - 30px) / 4);
+      @media screen and (max-width: 790px) {
+         width: calc((100% - 10px) / 2);
+      }
+      @media screen and (max-width: 560px) {
+         width: 100%;
+      }
+
+      .imgWrap {
+         width: 100%;
+         position: relative;
+         overflow: hidden;
+         cursor: pointer;
+         border-radius: $radius;
+
+         img {
+            width: 100%;
+            transition: 0.3s;
+         }
+         &:hover img {
+            transform: scale(1.1);
+         }
+         .hot {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            background-color: skyblue;
+            padding: 2px 10px;
+            border-radius: 5px;
+            font-weight: bold;
+            color: white;
+         }
+      }
+   }
+}
+
+.mb-10 {
+   margin-bottom: 10px;
+}
+
+.btn {
+   background: pink;
+   border-radius: $radius;
+   padding: 5px 16px;
+   text-align: center;
+   cursor: pointer;
+   color: white;
+   border: 0 none;
+   display: block;
+   width: 100%;
+
+   &.btn-primary {
+      background-color: skyblue;
+   }
+   &.btn-info {
+      background-color: greenyellow;
+   }
+}
+</style>
---
+++ 04vue/vuetest1/src/components/SearchBarView.vue
@@ -0,0 +1,41 @@
+<template>
+   <div class="container">
+      search
+      <!-- <input type="search" @input="inputText = $event.target.value" placeholder="검색어를 입력하세요." /> -->
+      <!-- <input type="search" v-model="inputText" placeholder=" 검색어를 입력하세요." /> -->
+      <input type="search" @change="inputText = $event.target.value" />
+
+      <button>검색</button>
+
+      <p>{{ inputText }}</p>
+      <!-- <p>{{ data }}</p> -->
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'SearchComp',
+   props: {
+      data: Object,
+   },
+   data() {
+      return {
+         inputText: 'test',
+      };
+   },
+   watch: {
+      inputText(name) {
+         console.log(name);
+         const findName = this.data.filter(movie => {
+            return movie.title.includes(name); // 타이틀이 있는지 없는지 확인해주는 코드
+         });
+         console.log(findName.length);
+         if (findName.length == 0) {
+            alert('영화자료가 없습니다 !!!');
+         }
+      },
+   },
+};
+</script>
+
+<style lang="scss" scoped></style>
