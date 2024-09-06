b8f89c96f8b01e2c067978427888f911358a4efa
zlnongi <yeossi8@naver.com>
2024-09-06 11:14:34 +0900

    vueproject2 t시작

---
+++ 04vue/vueproject2/src/store/store.js
@@ -0,0 +1,30 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    userData: [
+      {
+        userid: "zlnongi",
+        password: "123",
+        username: "여진홍",
+        addr: "서울",
+        iamge: "test.jpg",
+      },
+      {
+        userid: "ganggang",
+        password: "123",
+        username: "장윤경",
+        addr: "안양",
+        iamge: "test.jpg",
+      },
+      {
+        userid: "minalee",
+        password: "123",
+        username: "이민아",
+        addr: "서울",
+        iamge: "test.jpg",
+      },
+    ],
+  },
+  mutations: {},
+});
