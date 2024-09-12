31926018103beab943e35a1c9abd6d3934a5f670
zlnongi <yeossi8@naver.com>
2024-09-12 11:35:35 +0900

    pager basic

--- 07React/reacttail1/src/App.js
+++ 07React/reacttail1/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <div className="asdf"></div>
+    </div>
+  );
 }
 
 export default App;
