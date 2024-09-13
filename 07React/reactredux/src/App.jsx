d6546e9ed3387dcfb237fc624cca927fe5252974
zlnongi <yeossi8@naver.com>
2024-09-13 11:43:03 +0900

    react redux 시작

--- 07React/reactredux/src/App.jsx
+++ 07React/reactredux/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { createStore } from "redux";
+
+// -- store file
+const init = {
+  // init과 state을 연결시켜야함 state=init로 만들어줌 (initial data)
+  value: 100,
+  title: "test",
+};
+
+const reducer = (state = init, action) => {
+  // reducer라는 이름을 쓴거임 그냥
+
+  // 함수 -> 조건문, switch문 등 action
+  console.log(state);
+  console.log(action.type);
+  if (action.type == "up") {
+    return { ...state, value: state.value + action.payload };
+  }
+  if (action.type == "down") {
+    return { ...state, value: state.value - action.payload };
+  }
+
+  return state; // state값 변경
+};
+const store = createStore(reducer); // reducer가 state값인거임 (provider값?? 값을 변화시키는 중간 매체 = reducer)
+// -- store file
 
 function App() {
-  const [count, setCount] = useState(0)
+  return (
+    <Provider store={store}>
+      <div>test</div>
+      <Counter />
+    </Provider>
+  );
+}
+
+function Counter() {
+  //const counterValue = useSelector((state)=>{return state.value})
+  const counterValue = useSelector((state) => state.value); // data 가져옴
+  const titleValue = useSelector((state) => state.title);
+
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {counterValue} / {titleValue}
+      <button
+        onClick={() => {
+          dispatch({ type: "up", payload: 1 }); // data 반영, 변경
+        }}
+      >
+        클릭
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "down", payload: 1 });
+        }}
+      >
+        다운
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
