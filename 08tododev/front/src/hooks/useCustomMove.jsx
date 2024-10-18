599e7c922a764442247267d60d0a0d6f2b273b6a
zlnongi <yeossi8@naver.com>
2024-10-18 09:24:57 +0900

    useCustomMove

--- 08tododev/front/src/hooks/useCustomMove.jsx
+++ 08tododev/front/src/hooks/useCustomMove.jsx
@@ -2,28 +2,29 @@ import { useState } from "react";
 import {
   createSearchParams,
   useNavigate,
-  useSearchParams,
+  useSearchParams, // url의 쿼리 파라미터를 읽고 처리하는 기능을 제공하는 훅
 } from "react-router-dom";
 
 const getNum = (param, dValue) => {
   if (!param) {
     return dValue; // page: 1, size: 10으로 적은거
   }
-  return parseInt(param);
+  return parseInt(param); // 정수로 변환
 };
 
 const useCustomMove = () => {
-  const navigate = useNavigate();
-  const [refresh, setRefresh] = useState(false);
-  const [queryParams] = useSearchParams();
+  const navigate = useNavigate(); // 페이지 이동을 위한 훅
+  const [refresh, setRefresh] = useState(false); // 페이지 상태를 강제로 다시 그리기 위해 사용하는 상태값
+  const [queryParams] = useSearchParams(); // url 쿼리 파라미터를 가져옴 -> 여기서 page와 size 값 읽어옴
   const page = getNum(queryParams.get("page"), 1);
   const size = getNum(queryParams.get("size"), 10);
 
-  // page=2&size=10
+  // 기본 쿼리 문자열을 이런 형식으로 -> page=2&size=10 저장함
   const queryDefault = createSearchParams({ page, size }).toString();
 
   const moveToList = (pageParam) => {
-    let queryStr = "";
+    // 리스트 페이지로 이동하는 기능
+    let queryStr = ""; // queryStr 초기화. 쿼리 스트링을 저장할 변수를 빈 문자열로 초기화
     if (pageParam) {
       const pageNum = getNum(pageParam.page, 1);
       const sizeNum = getNum(pageParam.size, 1);
